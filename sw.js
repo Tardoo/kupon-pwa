939dc703b85095ffc8fce1806be49f650ebd3d77
Deniz Baha Ay <96914903+Tardoo@users.noreply.github.com>
2026-01-08 16:51:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("kupon-pwa").then(cache =>
-      cache.addAll([
-        "index.html",
-        "manifest.json"
-      ])
+      cache.addAll(["index.html","manifest.json"])
     )
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
